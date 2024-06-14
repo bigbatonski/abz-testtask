c037012925681a1e2239364c4a6bf8b25141efeb
David Ferreira <vcheckpoint09@gmail.com>
2024-06-14 15:28:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './sytles/styles.css'
-import { FetchProvider } from './components/context/RefetchUsersContext.jsx'
+import FetchProvider  from './components/context/RefetchUsersContext.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
